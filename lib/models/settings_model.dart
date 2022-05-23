7f723fc1e96b416b75241cf1372db89394c7b10c
kiiru4reals <smaina991@gmail.com>
2022-05-24 00:14:51 +0300

    To be deleted soon

--- lib/models/settings_model.dart
+++ lib/models/settings_model.dart
@@ -2,22 +2,15 @@ import 'package:hive/hive.dart';
 part 'settings_model.g.dart';
 
 @HiveType(typeId: 1)
-class Settings extends HiveObject {
+class SettingsModel extends HiveObject {
+  @HiveField(0)
+  final bool presets_time = true;
   @HiveField(1)
-  final bool presets_time;
+  final bool presets_place = false;
   @HiveField(2)
-  final bool presets_place;
+  final String alert_style = "Medium";
   @HiveField(3)
-  final String alert_style;
+  final bool show_reminder_on_app_screen = false;
   @HiveField(4)
-  final String show_reminder_on_app_screen;
-  @HiveField(5)
-  final bool notifications;
-
-  Settings(
-      {required this.presets_time,
-      required this.presets_place,
-      required this.alert_style,
-      required this.show_reminder_on_app_screen,
-      required this.notifications});
+  final bool notifications = true;
 }
