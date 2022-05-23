5b9e42763bd891b1fc38a30069a7c58dc6e01b74
kiiru4reals <smaina991@gmail.com>
2022-05-24 01:25:30 +0300

    Refactoring

--- lib/features/saved_links/saved_links.dart
+++ lib/features/saved_links/saved_links.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:soluprov/features/saved_links/routes/add_links.dart';
 
 class SavedLinks extends StatefulWidget {
   const SavedLinks({Key? key}) : super(key: key);
@@ -11,6 +12,14 @@ class _SavedLinksState extends State<SavedLinks> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text("Your links"),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => Navigator.of(context).push(MaterialPageRoute(builder: (context) => AddLinks())),
+        child: Icon(Icons.add),
+      ),
       body: Center(child: Text("Save your links here!"),),
     );
   }
