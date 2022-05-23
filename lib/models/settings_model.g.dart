e3657168eea89d3d10ecb7a8dda499bff5dee97c
kiiru4reals <smaina991@gmail.com>
2022-05-24 00:16:17 +0300

    Initial commit

---
+++ lib/models/settings_model.g.dart
@@ -0,0 +1,43 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'settings_model.dart';
+
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class SettingsModelAdapter extends TypeAdapter<SettingsModel> {
+  @override
+  final int typeId = 1;
+
+  @override
+  SettingsModel read(BinaryReader reader) {
+    return SettingsModel();
+  }
+
+  @override
+  void write(BinaryWriter writer, SettingsModel obj) {
+    writer
+      ..writeByte(5)
+      ..writeByte(0)
+      ..write(obj.presets_time)
+      ..writeByte(1)
+      ..write(obj.presets_place)
+      ..writeByte(2)
+      ..write(obj.alert_style)
+      ..writeByte(3)
+      ..write(obj.show_reminder_on_app_screen)
+      ..writeByte(4)
+      ..write(obj.notifications);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is SettingsModelAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
