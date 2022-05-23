8602a7782694d739003e97865475b970472bddf6
kiiru4reals <smaina991@gmail.com>
2022-05-24 00:11:48 +0300

    Integrated hive to settings

--- lib/features/settings/widgets/general_settings.dart
+++ lib/features/settings/widgets/general_settings.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:list_tile_switch/list_tile_switch.dart';
-import 'package:soluprov/config.dart';
+import 'package:provider/provider.dart';
+import 'package:soluprov/provider/settings/settings_provider.dart';
 
 class GeneralSettings extends StatefulWidget {
   const GeneralSettings({Key? key}) : super(key: key);
@@ -12,35 +13,44 @@ class GeneralSettings extends StatefulWidget {
 class _GeneralSettingsState extends State<GeneralSettings> {
   @override
   Widget build(BuildContext context) {
-    bool _value = true;
+    final provider = Provider.of<SettingsProvider>(context);
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         Padding(
           padding: const EdgeInsets.all(12.0),
-          child: Text("General",style: TextStyle(fontSize: 14,
-          fontWeight: FontWeight.bold,
-          color: Colors.grey),),
+          child: Text(
+            "General",
+            style: TextStyle(
+                fontSize: 14, fontWeight: FontWeight.bold, color: Colors.grey),
+          ),
         ),
         Material(
           color: Colors.transparent,
           child: InkWell(
             splashColor: Colors.black12,
             child: InkWell(
-              onTap: (){},
+              onTap: () {},
               child: ListTile(
-                onTap: (){},
-                title: Text("Alert style ",),
-                subtitle: Text("Medium", style: TextStyle(color: Colors.purple) ,),
+                onTap: () {},
+                title: Text(
+                  "Alert style ",
+                ),
+                subtitle: Text(
+                  "Medium",
+                  style: TextStyle(color: Colors.purple),
+                ),
               ),
             ),
           ),
         ),
         ListTileSwitch(
-          value: _value,
-          onChanged: (bool value) { setState(() {
-            value = true;
-          }); },
+          value: provider.showreminderonappScreen,
+          onChanged: (value) {
+            setState(() {
+              provider.showreminderonappScreen = value;
+            });
+          },
           switchActiveColor: Colors.indigo,
           title: Text("Show reminder on app screen"),
         ),
