4b29f32993e364bcde428a2be2101285cc35d0e0
kiiru4reals <smaina991@gmail.com>
2022-05-24 13:16:26 +0300

    UI development

--- lib/features/events/routes/event_details.dart
+++ lib/features/events/routes/event_details.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:list_tile_switch/list_tile_switch.dart';
+import 'package:soluprov/main.dart';
 import 'package:soluprov/models/event_model.dart';
 
 class EventDetailsScreen extends StatefulWidget {
@@ -10,20 +12,49 @@ class EventDetailsScreen extends StatefulWidget {
 }
 
 class _EventDetailsScreenState extends State<EventDetailsScreen> {
-  late final Event eventinfo;
   @override
   Widget build(BuildContext context) {
+    bool value = false;
     return Scaffold(
       appBar: AppBar(
-        title: Text("Title of the event", maxLines: 2,
+        title: Text("Event title", maxLines: 1,
         overflow: TextOverflow.ellipsis,),
         centerTitle: true,
         actions: [
-
+          IconButton(onPressed: (){}, icon: Icon(Icons.delete)),
+          IconButton(onPressed: (){}, icon: Icon(Icons.edit)),
         ],
       ),
       body: Column(
-        children: [Text("")],
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(13.0),
+            child: Text("Description of the event"),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(13.0),
+            child: Text("Start time"),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(13.0),
+            child: Text("End time"),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(13.0),
+            child: Material(
+              child: ListTileSwitch(value: value, onChanged: (value) {
+                setState(() {
+                  value = value;
+                });
+              },
+              switchActiveColor: Colors.purple,
+                title: Text("Completed"),
+                visualDensity: VisualDensity.comfortable,
+              ),
+            ),
+          ),
+        ],
       )
     );
   }
