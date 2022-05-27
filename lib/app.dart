25ea27c4b527f1907dc096928e0c095d42099be3
kiiru4reals <smaina991@gmail.com>
2022-05-27 13:18:47 +0300

    Imported provider, tasks and settings provider

--- lib/app.dart
+++ lib/app.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:soluprov/core/styles.dart';
+import 'package:soluprov/features/settings/services/settings_provider.dart';
 import 'package:soluprov/features/splashscreen/splash_screen.dart';
+import 'package:soluprov/features/tasks/services/task_provider.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
