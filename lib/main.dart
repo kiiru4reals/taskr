2f3baaf317f2ff552d61c3deb3ddbed5b9d59a7d
kiiru4reals <smaina991@gmail.com>
2022-05-27 14:59:44 +0300

    Removed useless imports

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:hive_flutter/hive_flutter.dart';
-import 'package:provider/provider.dart';
 import 'package:soluprov/app.dart';
-import 'package:soluprov/core/styles.dart';
-import 'package:soluprov/features/splashscreen/splash_screen.dart';
-import 'package:soluprov/features/tasks/services/task_provider.dart';
-import 'package:soluprov/features/tasks/models/task_model.dart';
-import 'package:soluprov/features/settings/models/settings_model.dart';
-import 'package:soluprov/features/settings/services/settings_provider.dart';
 import 'package:soluprov/init.dart';
 
 
