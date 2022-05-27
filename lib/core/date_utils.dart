aa62be43d0122ee62adad40871d686d4d348c767
kiiru4reals <smaina991@gmail.com>
2022-05-27 14:40:25 +0300

    Removed unnecessary string interpolation

--- lib/core/date_utils.dart
+++ lib/core/date_utils.dart
@@ -10,12 +10,12 @@ class DateUtil{
   static String toDate(DateTime dateTime){
     final date = DateFormat.yMMMEd().format(dateTime);
 
-    return "$date";
+    return date;
   }
   static String toTime (DateTime dateTime){
     final time = DateFormat.Hm().format(dateTime);
 
-    return "$time";
+    return time;
   }
   static DateTime removeTime (DateTime dateTime) =>
       DateTime(dateTime.year, dateTime.month, dateTime.day);
