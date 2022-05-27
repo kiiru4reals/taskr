b9f074819fd5594e630dfff8baa85c41fc21a098
kiiru4reals <smaina991@gmail.com>
2022-05-27 13:15:03 +0300

    Regenerated after changing event_model.dart to task_model.dart

---
+++ lib/features/tasks/models/task_model.g.dart
@@ -0,0 +1,56 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'task_model.dart';
+
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class TaskAdapter extends TypeAdapter<Task> {
+  @override
+  final int typeId = 0;
+
+  @override
+  Task read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return Task(
+      title: fields[0] as String,
+      description: fields[1] as String,
+      startDateTime: fields[2] as DateTime,
+      toDateTime: fields[3] as DateTime,
+      priority: fields[5] as String,
+      isAllDay: fields[6] as bool,
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, Task obj) {
+    writer
+      ..writeByte(6)
+      ..writeByte(0)
+      ..write(obj.title)
+      ..writeByte(1)
+      ..write(obj.description)
+      ..writeByte(2)
+      ..write(obj.startDateTime)
+      ..writeByte(3)
+      ..write(obj.toDateTime)
+      ..writeByte(5)
+      ..write(obj.priority)
+      ..writeByte(6)
+      ..write(obj.isAllDay);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is TaskAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
