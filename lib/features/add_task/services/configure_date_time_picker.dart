23627f1e19665eed4d32d434b48798a1598969d7
kiiru4reals <smaina991@gmail.com>
2022-05-30 09:49:18 +0300

    initial commit

---
+++ lib/features/add_task/services/configure_date_time_picker.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class ConfigureDateTimePickerProvider extends ChangeNotifier{
+  late DateTime fromDate;
+  late DateTime toDate;
+
+
+  Future pickFromDateTime({required bool pickDate}) async {
+    final date = await pickDateTime(fromDate,
+        pickDate: pickDate, firstDate: pickDate ? fromDate : null);
+    if (date == null) return;
+    fromDate = date;
+
+    if (date.isAfter(toDate)) {
+      toDate =
+          DateTime(date.year, date.month, date.day, toDate.hour, toDate.minute);
+    }
+  }
+
+  Future<DateTime?> pickDateTime(
+      DateTime initialDate, {
+        required bool pickDate,
+        DateTime? firstDate,
+      }) async {
+    if (pickDate) {
+      var context;
+      final date = await showDatePicker(
+          context: context,
+          initialDate: initialDate,
+          firstDate: firstDate ?? DateTime(2015, 8),
+          lastDate: DateTime(2100));
+      if (date == null) return null;
+
+      final time =
+      Duration(hours: initialDate.hour, minutes: initialDate.minute);
+
+      return date.add(time);
+    } else {
+      var context;
+      final timeOfDay = await showTimePicker(
+          context: context, initialTime: TimeOfDay.fromDateTime(initialDate));
+      if (timeOfDay == null) return null;
+      final date =
+      DateTime(initialDate.year, initialDate.month, initialDate.day);
+      final time = Duration(hours: timeOfDay.hour, minutes: timeOfDay.minute);
+      return date.add(time);
+    }
+  }
+}
