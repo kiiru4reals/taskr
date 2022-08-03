34657d438041e999aa29ab55482e85d812ff341b
kiiru4reals <smaina991@gmail.com>
2022-08-03 10:15:56 +0300

    moved bottom_bar.dart to core/presentation/BottomBar

--- lib/features/splashscreen/splash_screen.dart
+++ lib/features/splashscreen/splash_screen.dart
@@ -1,7 +1,7 @@
 import 'package:easy_splash_screen/easy_splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:soluprov/core/widgets/BottomNavigationBar/bottom_bar.dart';
+import 'package:soluprov/core/presentation/BottomNavigationBar/bottom_bar.dart';
 import 'package:soluprov/features/settings/services/settings_provider.dart';
 import 'package:soluprov/features/tasks/services/task_provider.dart';
 
@@ -15,7 +15,6 @@ class SplashScreen extends StatefulWidget {
 class _SplashScreenState extends State<SplashScreen> {
   double value = 0;
 
-
   @override
   Widget build(BuildContext context) {
     final eventsProvider = Provider.of<TaskProvider>(context);
@@ -23,22 +22,22 @@ class _SplashScreenState extends State<SplashScreen> {
     eventsProvider.getTasks();
     settingsProvider.getSettings();
     return Scaffold(
-        body: EasySplashScreen(
-          logo: Image.network(
-              'https://cdn4.iconfinder.com/data/icons/logos-brands-5/24/flutter-512.png'),
-          title: const Text(
-            "Taskr- the app",
-            style: TextStyle(
-              fontSize: 18,
-              fontWeight: FontWeight.bold,
-            ),
+      body: EasySplashScreen(
+        logo: Image.network(
+            'https://cdn4.iconfinder.com/data/icons/logos-brands-5/24/flutter-512.png'),
+        title: const Text(
+          "Taskr- the app",
+          style: TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
           ),
-          backgroundColor: Colors.grey.shade400,
-          showLoader: true,
-          loadingText: const Text("Setting up"),
-          navigator: const BottomBarScreen(),
-          durationInSeconds: 20,
         ),
+        backgroundColor: Colors.grey.shade400,
+        showLoader: true,
+        loadingText: const Text("Setting up"),
+        navigator: const BottomBarScreen(),
+        durationInSeconds: 20,
+      ),
     );
   }
 }
