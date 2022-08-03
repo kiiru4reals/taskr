0e4cc18ac2f8675a96972a494883732fd5c86d64
kiiru4reals <smaina991@gmail.com>
2022-08-03 09:58:31 +0300

    renamed to event_local_data_source.dart and changed class name to EventLocalDataSource

--- lib/core/utils/event_local_data_source.dart
+++ lib/core/utils/event_local_data_source.dart
@@ -1,8 +1,8 @@
 import 'package:soluprov/features/tasks/models/task_model.dart';
 import 'package:syncfusion_flutter_calendar/calendar.dart';
 
-class EventDataSource extends CalendarDataSource{
-  EventDataSource(List<Task>appointments) {
+class EventLocalDataSource extends CalendarDataSource {
+  EventLocalDataSource(List<Task> appointments) {
     this.appointments = appointments;
   }
   Task getEvent(int index) => appointments![index] as Task;
@@ -11,12 +11,11 @@ class EventDataSource extends CalendarDataSource{
   DateTime getStartTime(int index) => getEvent(index).startDateTime;
 
   @override
-  DateTime getEndTime (int index) => getEvent(index).toDateTime;
+  DateTime getEndTime(int index) => getEvent(index).toDateTime;
 
   @override
-  String getSubject (int index) => getEvent(index).title;
+  String getSubject(int index) => getEvent(index).title;
 
   @override
-  bool isAllDay (int index) => getEvent(index).isAllDay;
-
+  bool isAllDay(int index) => getEvent(index).isAllDay;
 }
