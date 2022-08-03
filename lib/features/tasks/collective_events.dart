108e0f6909b97057a5497458d59e9ae7a8401f5f
kiiru4reals <smaina991@gmail.com>
2022-08-03 12:56:49 +0300

    moved add_tasks.dart screen to features/presentation

--- lib/features/tasks/collective_events.dart
+++ lib/features/tasks/collective_events.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:soluprov/features/add_task/add_task.dart';
+import 'package:soluprov/features/add_task/presentation/pages/add_task.dart';
 import 'package:soluprov/features/tasks/widget/calendar_widget.dart';
+
 class CollectiveEvents extends StatefulWidget {
   const CollectiveEvents({Key? key}) : super(key: key);
 
@@ -13,16 +14,18 @@ class _CollectiveEventsState extends State<CollectiveEvents> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Your events",),
+        title: const Text(
+          "Your events",
+        ),
         centerTitle: true,
         backgroundColor: Colors.purple,
       ),
       body: const CalendarWidget(),
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.add),
-        onPressed: () => Navigator.of(context).push(MaterialPageRoute(builder: (context) => const AddTasks())),
+        onPressed: () => Navigator.of(context)
+            .push(MaterialPageRoute(builder: (context) => const AddTasks())),
       ),
-
     );
   }
 }
